a91bb13f22969961f53285746c6fe31056aaadcb
aiboonyapon <boonyapon.m@kkumail.com>
2020-03-10 20:45:44 +0700

    commit

--- app/src/main/java/com/onkasem/smartco_workingbooking/Login.kt
+++ app/src/main/java/com/onkasem/smartco_workingbooking/Login.kt
@@ -3,14 +3,16 @@ package com.onkasem.smartco_workingbooking
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        mAuth = FirebaseAuth.getInstance();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
